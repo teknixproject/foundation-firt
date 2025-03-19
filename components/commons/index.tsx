a8451fcf43b7d8746171fbd037c552cc7ef64d1e
teknixproject <project@teknixcorp.com>
2025-03-19 17:18:27 +0700

    Update index - Add supportV2

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -14,4 +14,5 @@ export const components = {
   title_header: dynamic(() => import('./titleHeader')),
   title_header_gradient: dynamic(() => import('./titleHeaderGradient')),
   faq_v2: dynamic(() => import("./faqV2")),
+  support_v2: dynamic(() => import("./supportV2")),
 };
